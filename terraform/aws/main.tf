d174f65f8cfa7f4efbff315c568a6dd8e18d072e
Henrique <129874242+hikefran@users.noreply.github.com>
2023-11-22 21:56:10 -0300

    oaoaoo

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -16,7 +16,12 @@ resource "aws_subnet" "sn_pub_az1a" {
     cidr_block              = "10.0.1.0/24"
     map_public_ip_on_launch = true
 }
-
+resource "aws_subnet" "sn_pub_az1b" {
+    vpc_id                  = aws_vpc.vpc.id
+    availability_zone       = "us-east-1b"
+    cidr_block              = "10.0.2.0/24"
+    map_public_ip_on_launch = true
+}
 
 # RESOURCE: ROUTE TABLES FOR THE SUBNETS
 resource "aws_route_table" "rt_pub" {
